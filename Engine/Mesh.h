7a307a91973e3acf4ca4188b7fd5303486ea22f4
Im caheeun <dev.chaeng@gmail.com>
2025-06-09 02:56:56 +0900

    Particle System 완료

--- Engine/Mesh.h
+++ Engine/Mesh.h
@@ -11,7 +11,7 @@ public:
 	virtual ~Mesh();
 
 	void Init(const vector<Vertex>& vertexBuffer, const vector<uint32>& indexbuffer);
-	void Render();
+	void Render(uint32 instanceCount = 1);
 
 private:
 	void CreateVertexBuffer(const vector<Vertex>& buffer);
@@ -26,4 +26,3 @@ private:
 	D3D12_INDEX_BUFFER_VIEW		_indexBufferView;
 	uint32 _indexCount = 0;
 };
-
